12b961c6d1bb18f21fd76ede1db505195385e314
ruimin.wang <ruimin.wang@jointwisdom.cn>
2020-11-12 10:39:57 +0800

    update

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,11 +1,12 @@
+import React,{useEffect} from 'react'
 import './App.css';
-import Father from './components/father'
+import Tables from './components/table'
 function App() {
-  return (
-    <div className="App">
-        <Father/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Tables/>
+        </div>
+    );
 }
 
 export default App;
