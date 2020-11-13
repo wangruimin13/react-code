876640a4b4818ff33d1d41a74c90d9fa5f609016
ruimin.wang <ruimin.wang@jointwisdom.cn>
2020-11-13 11:19:50 +0800

    'commit'

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
-function Calculator(){
-    console.log(prompt)
-}
-let calculator = new Calculator();
-calculator.read();
+// function Calculator(){
+//     console.log(prompt)
+// }
+// let calculator = new Calculator();
+// calculator.read();
 
-alert( "Sum=" + calculator.sum() );
-alert( "Mul=" + calculator.mul() );
+// alert( "Sum=" + calculator.sum() );
+// alert( "Mul=" + calculator.mul() );
+// console.log('2020-01-01'.replace(/(\d*)-(\d*)-(\d*)/g,'$1年$2月$3日'))//2020年01月01日
+var i=1; console.log(i -= 1 && 1 + ++i);
